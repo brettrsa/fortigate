328ce39e3940bf9873b0a05bcf567609e36ed95b
brettrsa <brettrsa@gmail.com>
2014-02-06 15:26:23 +0200

    Update

---
+++ login_attempts/get_forti_logins.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+#
+# This is a simple perl script that gathers the login attempts to a 
+# fortigate device from the elog.log file on the fortigate device
+#
+# Thanks to Alan Houston for you assistance :)
+#
+# Start
+
+use strict;
+
+        # Open up the file from the command line
+
+        my $file = $ARGV[0];
+
+        open (IN,$file) or die "Couldn't open $file for reading: $!\n";
+
+        while (my $line = <IN>) {       # Loop through file
+
+           # (\d+\.\d+\.\d+\.\d+) = Regex for IP Address
+
+                if ($line=~/ user=(["]\w+["]).+ui=(\w+[(]\d+\.\d+\.\d+\.\d+[)]).+action=(\w+).+status=(\w+)/)  {
+
+                      printf "Username: $1 \tMethod and IP : $2  \tAction: $3 $4\n";
+
+                }
+
+        }
+
+# End
+
---
+++ traffic_logs/get_forti_traffic.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+#
+# This is a simple perl script that formats the data from a fortigate
+# tlog.log file into an easy to read format
+#
+# Thanks go out to Alan Houston for your help with this :)
+#
+# Start
+
+use strict;
+
+        # Open up the file from the command line
+
+        my $file = $ARGV[0];
+
+        open (IN,$file) or die "Couldn't open $file for reading: $!\n";
+
+        while (my $line = <IN>) {       # Loop through file
+
+                # (\d+\.\d+\.\d+\.\d+) = Regex for IP Address
+
+                if ($line=~/ src=(\d+\.\d+\.\d+\.\d+).+dst=(\d+\.\d+\.\d+\.\d+).+dst_port=(\d+)/) {
+
+                        printf "Source: $1 \tDest: $2 \tPort: $3\n";
+
+                }
+
+        }
+
+# End
